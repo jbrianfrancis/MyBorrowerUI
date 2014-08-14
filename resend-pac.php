319fff15f730021319e8d66489dd5b0421084b62
Buck <bucksharp@gmail.com>
2014-08-14 19:31:54 -0400

    Tweaks

--- resend-pac.php
+++ resend-pac.php
@@ -31,7 +31,7 @@
 <div class="container">
    	<form role="form" class="form-signin">
 	<div class="row white-header">
-		<img src="images/acctchk.svg" alt="ACCOUNTCHEK: Simple.Smart.Secure." onerror="this.onerror=null; this.src='ac-tagline-300.png'">
+    <img src="images/logo-check.svg" width="106" alt="ACCOUNTCHEK: Simple.Smart.Secure." onerror="this.onerror=null; this.src='images/logo-check.png'">
 	</div>
   <div class="panel panel-default panel-signin">
       <div class="panel-body panel-body-signin">
