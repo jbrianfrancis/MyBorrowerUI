bdd86008885fbeb0ac77927a7d2d9ecf897d3043
Buck <bucksharp@gmail.com>
2014-08-18 10:00:51 -0400

    Adding new view to index

--- index.php
+++ index.php
@@ -99,6 +99,10 @@
             <h4 class="list-group-item-heading">Banks Enrolled</h4>
             <p class="list-group-item-text"></p>
           </a>
+          <a href="banks-enrolled-continue.php" class="list-group-item">
+            <h4 class="list-group-item-heading">Banks Enrolled with Additional</h4>
+            <p class="list-group-item-text"></p>
+          </a>
           <a href="banks-enrolled-logout.php" class="list-group-item">
             <h4 class="list-group-item-heading">Banks Enrolled Sign Out</h4>
             <p class="list-group-item-text"></p>
