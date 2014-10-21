ec5e0fca9c5d69cb2a0d2e00f1a2f115d7de2a67
Brian Francis <jbrianfrancis@hotmail.com>
2014-10-21 14:26:50 -0400

    one second refresh

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
 <?php  
 //this will NOT work, the browser received the HTML tag before the script   
-header( 'Refresh: 0;walkthrough/sign-in.php' );  
+header( 'Refresh: 1;walkthrough/sign-in.php' );  
 ?>
