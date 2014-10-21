fbc45069adb8f9f7c76c077d7fe673ee22bbe76d
Brian Francis <jbrianfrancis@hotmail.com>
2014-10-21 14:25:20 -0400

    Using Refresh to redirect

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
 <?php  
 //this will NOT work, the browser received the HTML tag before the script   
-header( 'Location: walkthrough/sign-in.php' );  
+header( 'Refresh: 0;walkthrough/sign-in.php' );  
 ?>
